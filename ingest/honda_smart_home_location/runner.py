dd9a5eae7ce4b5e680637f74ec8526b2c5f04331
jmcvey3 <jmcvey3@users.noreply.github.com>
2022-03-24 17:44:51 -0700

    yml file update

--- ingest/honda_smart_home_location/runner.py
+++ ingest/honda_smart_home_location/runner.py
@@ -8,5 +8,6 @@ if __name__ == "__main__":
         expand("config/pipeline_config_honda_smart_home_location.yml", __file__),
         expand("config/storage_config_honda_smart_home_location.yml", __file__),
     )
-    #pipeline.run(expand("input_data/Honda_AllData0420_analyzed.csv", __file__))
-    pipeline.run(expand("input_data/test.csv", __file__))
+
+    pipeline.run(expand("input_data/Honda_AllData0420_analyzed.csv", __file__))
+    #pipeline.run(expand("input_data/test.csv", __file__))
